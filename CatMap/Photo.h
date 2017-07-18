e60362991927d17e6ecb9bb53016220ab8f94509
Sam Ogunyowa <mailsamog@gmail.com>
2017-07-18 18:06:44 -0400

    update

--- CatMap/Photo.h
+++ CatMap/Photo.h
@@ -7,7 +7,17 @@
 //
 
 #import <Foundation/Foundation.h>
+@import UIKit;
 
 @interface Photo : NSObject
+@property (nonatomic, strong) NSString *urlServer;
+@property (nonatomic, strong) NSString *urlFarm;
+@property (nonatomic, strong) NSString *urlID;
+@property (nonatomic, strong) NSString *urlSecret;
+@property (nonatomic, strong) NSString *urlTitle;
+@property (nonatomic, strong) UIImage *catImage;
 
+
+- (instancetype)initWithInfo:(NSDictionary *)info;
+- (NSURL *)url;
 @end
